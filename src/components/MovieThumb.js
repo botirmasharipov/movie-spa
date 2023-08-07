7a2936c1114a8f9f2b48f32aec34d92e458edbc8
botirmasharipov <masbotirjon963@gmail.com>
2023-08-07 19:10:57 -0400

    create clickable movie thumb

--- src/components/MovieThumb.js
+++ src/components/MovieThumb.js
@@ -1,3 +1,12 @@
-const MovieThumb = () => <div>MovieThumb</div>;
+import {StyledMovieThumb} from '../styles/StyledMovieThumb';
 
+const MovieThumb = ({image, movieId, clickable}) => (
+    <StyledMovieThumb>
+    {clickable ? (
+        <img className="clickable" src={image} alt="moviethumsb" /> 
+    ): (
+      <img src={image} alt="moviethumb" />  //not clickable image
+    )}
+    </StyledMovieThumb>
+)
 export default MovieThumb;
