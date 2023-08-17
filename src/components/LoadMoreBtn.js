c4b9ee435f397e665a7b8b702cbdc2964243197a
botirmasharipov <masbotirjon963@gmail.com>
2023-08-17 11:28:36 -0400

    edit load more button styling

--- src/components/LoadMoreBtn.js
+++ src/components/LoadMoreBtn.js
@@ -1,3 +1,9 @@
-const LoadMoreBtn = () => <div>Load More</div>;
+import {Wrapper} from '../styles/StyledLoadMoreBtn';
+
+const LoadMoreBtn = ({text}) => (
+  <Wrapper type="button">
+    {text}
+  </Wrapper>
+);
 
 export default LoadMoreBtn;
